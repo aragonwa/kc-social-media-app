fcf9912f74b704e44c708a7e5a663b86ee03d16f
Aragon <aragonwa@gmail.com>
2018-04-05 08:43:26 -0700

    Adding init data

---
+++ src/db/data.js
@@ -0,0 +1,599 @@
+export const categories = [
+  {
+    name: 'main',
+    displayName: 'Main'
+  },
+  {
+    name: 'council',
+    displayName: 'County Council'
+  },
+  {
+    name: 'transportation',
+    displayName: 'Transportation'
+  },
+  {
+    name: 'public-health-human-services',
+    displayName: 'Public Health & Human Services'
+  },
+  {
+    name: 'public-safety',
+    displayName: 'Public Safety'
+  },
+  {
+    name: 'courts-justice',
+    displayName: 'Courts & Justice'
+  },
+  {
+    name: 'natural-resources-parks',
+    displayName: 'Natural Resources & Parks'
+  },
+  {
+    name: 'arts-culture-records',
+    displayName: 'Arts, Culture & Records'
+  },
+  {
+    name: 'elections',
+    displayName: 'Elections'
+  },
+  {
+    name: 'pets',
+    displayName: 'Pets'
+  },
+  {
+    name: 'assessor',
+    displayName: 'Assessor'
+  },
+  {
+    name: 'employees',
+    displayName: 'King County Employees'
+  }
+];
+export const platforms = [
+  {
+    name: 'twitter',
+    displayName: 'Twitter'
+  },
+  {
+    name: 'facebook',
+    displayName: 'Facebook'
+  },
+  {
+    name: 'instagram',
+    displayName: 'Instagram'
+  },
+  {
+    name: 'linkedin',
+    displayName: 'LinkedIn'
+  },
+  {
+    name: 'youtube',
+    displayName: 'Youtube'
+  }
+];
+
+const accounts = [
+  {
+    name: 'King County News',
+    networks: [
+      { type: 'facebook', url: 'https://www.kingcounty.gov' },
+      { type: 'twitter', url: 'https://www.kingcounty.gov' }
+    ],
+    categories: ['main']
+  },
+  {
+    name: 'Executive Dow Constantine',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['main']
+  },
+  {
+    name: 'King County Council',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['main']
+  },
+  {
+    name: 'Councilmember Rod Dembowski',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Larry Gossett',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Kathy Lambert',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Jeanne Kohl-Welles',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Dave Upthegrove',
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Claudia Balducci',
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Pete von Reichbauer',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Joe McDermott',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'Councilmember Reagan Dunn',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['council']
+  },
+  {
+    name: 'King County Metro Transit',
+    networks: [
+      {type: 'instagram', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'linkedin', url: 'https://www.kingcounty.gov'},
+
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'King County International Airport',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'King County Department of Transportation',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'King County In Motion',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'King County Water Taxi',
+    networks: [
+      {type: 'instagram', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'Rideshare Commuter Van Program	',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'King County Road Services',
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['transportation']
+  },
+  {
+    name: 'Public Health - Seattle & King County',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['public-health-human-services']
+  },
+  {
+    name: 'Listo Calixto en King County',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-health-human-services']
+  },
+  {
+    name: 'All Home King County',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-health-human-services']
+  },
+  {
+    name: 'Best Start for Kids',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-health-human-services']
+  },
+  {
+    name: 'King County Medic One',
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Sheriff's Office`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Search and Rescue`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Sheriff's Office 9-1-1`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Sheriff's Air Support`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Sheriff's Marine Rescue`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `E-911 Program`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `King County Office of Law Enforcement Oversight`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `Puget Sound Emergency Radio Network (PSERN)`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['public-safety']
+  },
+  {
+    name: `Prosecuting Attorney's Office (PAO)`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'linkedin', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['courts-justice']
+  },
+  {
+    name: `King County Superior Court`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['courts-justice']
+  },
+  {
+    name: `King County Youth Justice`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['courts-justice']
+  },
+  {
+    name: `King County Parks`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Natural Resources & Parks (DNRP)`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Rivers`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Wastewater Treatment Division (WTD)`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Brightwater Center`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Loop Biosolids`,
+    networks: [
+      {type: 'youtube', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `What's Up with Stormwater?`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Solid Waste Division (SWD)`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Recicla más`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `School Green Teams`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `EcoConsumer`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Rainwise`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Noxious Weeds`,
+    networks: [
+      {type: 'instagram', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `Recycle More`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['natural-resources-parks']
+  },
+  {
+    name: `4Culture`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['arts-culture-records']
+  },
+  {
+    name: `King County Historic Preservation`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['arts-culture-records']
+  },
+  {
+    name: `Archives`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['arts-culture-records']
+  },
+  {
+    name: `King County Auditor`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['arts-culture-records']
+  },
+  {
+    name: `Elections`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'instagram', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['elections']
+  },
+  {
+    name: `Regional Animal Services (RASKC)`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['pets']
+  },
+  {
+    name: `Assessor's Office`,
+    networks: [
+      {type: 'facebook', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['assessor']
+  },
+  {
+    name: `Employee News`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['employees']
+  },
+  {
+    name: `Careers at King County`,
+    networks: [
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+    ],
+    categories: ['employees']
+  },
+  {
+    name: 'King County TV',
+    networks: [
+      {type: 'youtube', url: 'https://www.kingcounty.gov'},
+      {type: 'twitter', url: 'https://www.kingcounty.gov'},
+      {type: 'facebook', url: 'https://www.kingcounty.gov'}
+    ],
+    categories: ['employees']
+  },
+  
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+  // {
+  //   name: '',
+  //   networks: [
+  //     {type: '', url: ''}
+  //   ],
+  //   categories: ['']
+  // },
+
+];
+
+export default accounts;
